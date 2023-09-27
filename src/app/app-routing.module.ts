c04f32c7ff4781ae878a435bffc6a3580fd2c374
Ivaylo Tsochev <ivo.tsochev@live.com>
2023-09-27 10:27:09 +0300

    NavBar
    - new centered-wrapper component for nesting the actual components
    - homepage component
    - working on the ShipmentCard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { ShipmentCardComponent } from './shipment-card/shipment-card/shipment-card.component'
+import { ShipmentCardComponent } from './shipment-card/shipment-card/shipment-card.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   { path: 'shipment-card', component: ShipmentCardComponent },
-]
+  { path: '', component: HomepageComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { ShipmentCardComponent } from './shipment-card/shipment-card/shipment-card.component'
-import { NavbarComponent } from './navbar/navbar.component'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShipmentCardComponent } from './shipment-card/shipment-card/shipment-card.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-import { MenubarModule } from 'primeng/menubar'
+import { MenubarModule } from 'primeng/menubar';
+import { TagModule } from 'primeng/tag';
+import { CenteredWrapperComponent } from './centered-wrapper/centered-wrapper.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
-  declarations: [AppComponent, ShipmentCardComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, MenubarModule],
+  declarations: [AppComponent, ShipmentCardComponent, NavbarComponent, CenteredWrapperComponent, HomepageComponent],
+  imports: [BrowserModule, AppRoutingModule, MenubarModule, TagModule],
   providers: [],
   bootstrap: [AppComponent],
 })
